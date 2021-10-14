b18f2b8214aacbefa50a97b1ab493f87141afa03
Andrews Micael <endrewsmicael@gmail.com>
2021-10-13 22:22:23 -0300

    quase feito

---
+++ func201.c
@@ -0,0 +1,19 @@
+/*Faça um programa com uma função chamada somaImposto. 
+A função possui dois parâmetros formais: taxaImposto, que é 
+a quantia de imposto sobre vendas expressa em porcentagem e custo, 
+que é o custo de um item antes do imposto. A função “altera” o 
+valor de custo para incluir o imposto sobre vendas.*/
+#include <stdlib.h>
+void somaimposto(taxaimposto, custo);
+int main(){
+    float taxaimposto, custo;
+    printf("A taxa de imposto: ");
+    scanf("%f",&taxaimposto);
+    printf("O custo: ");
+    scanf("%f",&custo);
+    somaimposto(taxaimposto, custo);
+}
+void somaimposto(taxaimposto, custo){
+    float somaimposto = custo+ (custo*taxaimposto/1008);
+    printf("\nResultado %.2f", somaimposto);
+}
---
+++ func203.c
@@ -0,0 +1,49 @@
+/*Faça um programa que use a função valorPagamento para determinar 
+o valor a ser pago por uma prestação de uma conta. O programa 
+deverá solicitar ao usuário o valor da prestação e o número de 
+dias em atraso e passar estes valores para a função valorPagamento, 
+que calculará o valor a ser pago e devolverá este valor ao 
+programa que a chamou. O programa deverá então exibir o valor a 
+ser pago na tela. Após a execução o programa deverá voltar a pedir 
+outro valor de prestação e assim continuar até que seja informado 
+um valor igual a zero para a prestação. Neste momento o programa 
+deverá ser encerrado, exibindo o relatório do dia, que conterá a 
+quantidade e o valor total de prestações pagas no dia. O cálculo do 
+valor a ser pago é feito da seguinte forma. Para pagamentos sem atraso, 
+cobrar o valor da prestação. Quando houver atraso, cobrar 
+3% de multa, mais 0,1% de juros por dia de atraso.*/
+#include <stdlib.h>
+void valorPagamento(vlPrestacao, nuatraso, multa, juro, valorpagar);
+int main(){
+    int vlPrestacao=1, nuatraso = 0, total = 0;
+    float valorpagar=0;
+    float multa, juro;
+    while(vlPrestacao != 0){
+        system("cls");
+        printf("\n\n*## CALCULO DE PRESTACOES ##*\n## PARA SAIR DIGITE '0' ##\n");
+        printf("\nO valor da prestacao> ");
+        scanf("%f", &vlPrestacao);
+        if(vlPrestacao == 0){
+            system("cls");
+            printf("**** RELATORIO ****\n");
+            printf("\nTotal de prestacoes: %d", total);
+            printf("\nValor total: R$%.2f", valorpagar);
+            printf("\nTchau!");
+            exit(0);
+        }
+        printf("\nNumero de dias de atraso> ");
+        scanf("%d", &nuatraso);
+        valorPagamento(vlPrestacao, nuatraso, multa, juro, valorpagar);
+        total++;
+    }
+}
+void valorPagamento(vlPrestacao, nuatraso, multa, juro, valorpagar){
+    if(nuatraso == 0){
+        valorpagar += vlPrestacao;
+    }
+    else{
+        multa = vlPrestacao * 0.03;
+        juro = vlPrestacao * (0.001*nuatraso);
+        valorpagar += vlPrestacao + multa + juro;
+    }
+}
